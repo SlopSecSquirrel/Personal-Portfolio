3c03fed9fe9affc904d49eae3be57b72a0ef5a32
Oliver Brooks <oliver.brooks@localhost.localdomain>
2023-10-15 11:45:54 -0400

    Quick bugfix on Main like a #rebel

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -71,7 +71,7 @@ export default async function Navbar() {
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 <Image
-                  src={"./github.svg"}
+                  src={"/github.svg"}
                   width={32}
                   height={32}
                   alt="GitHub icon"
